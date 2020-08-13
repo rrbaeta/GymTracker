90d188b6f8752f1c3fb6198f3d186cdc7afef101
rrbaeta <prorrb69DJ!>
2020-08-13 19:58:39 +0100

    added a Settings button the toolbar and on click it takes the user to the SettingsFragment

--- app/src/main/java/com/example/gymtracker/Activity/MainActivity.java
+++ app/src/main/java/com/example/gymtracker/Activity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.gymtracker.Activity;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.gymtracker.R;
@@ -16,20 +17,63 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         setUpNavigation();
     }
 
 
-
     //bottom bar navigation set up
     public void setUpNavigation(){
+
+        //find NavController
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        //setting up Toolbar with NavController
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        //setting up bottomNavigationView
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    //Used for toolbar button clicks. (Not in this case)
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()){
+//
+//            case R.id.nav_settings:
+//                Toast.makeText(getApplicationContext(), "Settings Selected", Toast.LENGTH_SHORT).show();
+//                Log.d("debug","fragment: action settings was clicked");
+//
+//                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new SettingsFragment()).addToBackStack(null).commit();
+//
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item);
+    }
+
 }
